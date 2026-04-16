bbd77e70a06723dffb3d5a4cbc1ab8656eb6c6da
dongwoo lee <138303561+merrybabyxmas@users.noreply.github.com>
2026-04-17 00:07:37 +0900

    Add Phase 65 adapters package init

---
+++ phase65_min3d/adapters/__init__.py
@@ -0,0 +1,11 @@
+from .base import BaseSceneAdapter
+from .decoder_adapter import DecoderSceneAdapter
+from .animatediff import AnimateDiffSceneAdapter
+from .sdxl import SDXLSceneAdapter
+
+__all__ = [
+    "BaseSceneAdapter",
+    "DecoderSceneAdapter",
+    "AnimateDiffSceneAdapter",
+    "SDXLSceneAdapter",
+]
